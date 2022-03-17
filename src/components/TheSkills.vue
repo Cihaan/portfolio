1036fb4f973c04c917db9dbadeff0feb9beac0f3
Cihaan <80070465+Cihaan@users.noreply.github.com>
2022-03-17 15:59:10 +0100

    skill composable

--- src/components/TheSkills.vue
+++ src/components/TheSkills.vue
@@ -1,63 +1,8 @@
 <script setup lang="ts">
 import TheSkill from './utils/TheSkill.vue';
+import skills from '../composable/skills';
 
-
-const skills = [
-  {
-    skillName: "Vue",
-    imagePath: "@/assets/icons/skills/vue.svg",
-    paragraph: "Advanced understanding of the framework",
-    caption: "vue icon"
-  },
-  {
-    skillName: "Javascript",
-    imagePath: "@/assets/icons/skills/javascript.svg",
-    paragraph: "Intermediate understanding of the language",
-    caption: "javascript icon"
-  },
-  {
-    skillName: "PostgreSQL",
-    imagePath: "@/assets/icons/skills/postgresql.svg",
-    paragraph: "Capable of advanced queries",
-    caption: "postgresql icon"
-  },
-  {
-    skillName: "Prisma",
-    imagePath: "@/assets/icons/skills/prisma.svg",
-    paragraph: "Good understanding and using of the ORM",
-    caption: "prisma icon"
-  },
-  {
-    skillName: "Java",
-    imagePath: "@/assets/icons/skills/java.svg",
-    paragraph: "Intermediate understanding of the language",
-    caption: "java icon"
-  },
-  {
-    skillName: "NodeJS",
-    imagePath: "@/assets/icons/skills/nodejs.svg",
-    paragraph: "Good understanding of the service",
-    caption: "nodejs icon"
-  },
-  {
-    skillName: "Docker",
-    imagePath: "@/assets/icons/skills/docker.svg",
-    paragraph: "Beginner at Docker",
-    caption: "docker icon"
-  },
-  {
-    skillName: "Angular",
-    imagePath: "../../assets/icons/skills/angular.svg",
-    paragraph: "Intermediate understanding of the framework",
-    caption: "angular icon"
-  },
-  {
-      skillName: "Vite",
-      imagePath: "@/assets/icons/skills/vite.svg",
-      paragraph: "Beginner at Vite",
-      caption: "vite icon"
-    },
-]
+let skillsList = skills.skills
 
 </script>
 
@@ -67,7 +12,7 @@ const skills = [
       <h2>Skills</h2>
     </div>
     <div class="skills-grid" >
-      <div v-for="skill in skills">
+      <div v-for="skill in skillsList">
         <TheSkill :image-path="skill.imagePath" :skill-name="skill.skillName" :paragraph="skill.paragraph" :caption="skill.caption" />
       </div>
     </div>
---
+++ src/composable/skills.ts
@@ -0,0 +1,58 @@
+export default {
+    skills: [
+        {
+          skillName: "Vue",
+          imagePath: "@/assets/icons/skills/vue.svg",
+          paragraph: "Advanced understanding of the framework",
+          caption: "vue icon"
+        },
+        {
+          skillName: "Javascript",
+          imagePath: "@/assets/icons/skills/javascript.svg",
+          paragraph: "Intermediate understanding of the language",
+          caption: "javascript icon"
+        },
+        {
+          skillName: "PostgreSQL",
+          imagePath: "@/assets/icons/skills/postgresql.svg",
+          paragraph: "Capable of advanced queries",
+          caption: "postgresql icon"
+        },
+        {
+          skillName: "Prisma",
+          imagePath: "@/assets/icons/skills/prisma.svg",
+          paragraph: "Good understanding and using of the ORM",
+          caption: "prisma icon"
+        },
+        {
+          skillName: "Java",
+          imagePath: "@/assets/icons/skills/java.svg",
+          paragraph: "Intermediate understanding of the language",
+          caption: "java icon"
+        },
+        {
+          skillName: "NodeJS",
+          imagePath: "@/assets/icons/skills/nodejs.svg",
+          paragraph: "Good understanding of the service",
+          caption: "nodejs icon"
+        },
+        {
+          skillName: "Docker",
+          imagePath: "@/assets/icons/skills/docker.svg",
+          paragraph: "Beginner at Docker",
+          caption: "docker icon"
+        },
+        {
+          skillName: "Angular",
+          imagePath: "../../assets/icons/skills/angular.svg",
+          paragraph: "Intermediate understanding of the framework",
+          caption: "angular icon"
+        },
+        {
+            skillName: "Vite",
+            imagePath: "@/assets/icons/skills/vite.svg",
+            paragraph: "Beginner at Vite",
+            caption: "vite icon"
+          },
+      ]
+}
